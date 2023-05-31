ec42ec6391daada0c44a2bc1c66d1cbaf1c224fc
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-05-31 17:55:19 +0300

    added .js to gamePlay

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // TODO: write code here
 import GameController from './GameController';
-import GamePlay from './GamePlay';
+import GamePlay from './GamePlay.js';
 import GenerateRandom from './randomNum';
 import Statistic from './Statistic';
 import Cursor from './Cursor';
